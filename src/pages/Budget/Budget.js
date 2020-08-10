eb318308ebf8d30a5548af2653faeb93f632a756
Konrad <konrad.gmur@gmail.com>
2020-08-10 20:19:11 +0200

    Change of logic in Budget.js

--- src/pages/Budget/Budget.js
+++ src/pages/Budget/Budget.js
@@ -1,27 +1,52 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useMemo } from "react";
 import { connect } from "react-redux";
 import {
   fetchBudget,
   fetchBudgetedCategories,
 } from "data/actions/budget.actions";
+import { fetchAllCategories } from "data/actions/common.actions";
+import { Grid } from "./Budget.css";
 
-function Budget({ fetchBudget, fetchBudgetedCategories }) {
+function Budget({
+  commonState,
+  budgetState,
+  fetchBudget,
+  fetchBudgetedCategories,
+  fetchAllCategories,
+}) {
   useEffect(() => {
     fetchBudget(1);
     fetchBudgetedCategories(1);
-  }, [fetchBudget, fetchBudgetedCategories]);
+    fetchAllCategories();
+  }, [fetchBudget, fetchBudgetedCategories, fetchAllCategories]);
+  const isLoaded = useMemo(
+    () =>
+      Object.keys(commonState).lenght === 0 &&
+      Object.keys(budgetState).length === 0,
+    [commonState, budgetState]
+  );
 
-  return <div>Budget</div>;
+  console.log(isLoaded);
+
+  return (
+    <Grid>
+      <section>Category List</section>
+      <section>Transaction List</section>
+    </Grid>
+  );
 }
 
 export default connect(
   (state) => {
     return {
       budget: state.budget.budget,
+      commonState: state.common.loadingState,
+      budgetState: state.budget.loadingState,
     };
   },
   {
     fetchBudget,
     fetchBudgetedCategories,
+    fetchAllCategories,
   }
 )(Budget);
