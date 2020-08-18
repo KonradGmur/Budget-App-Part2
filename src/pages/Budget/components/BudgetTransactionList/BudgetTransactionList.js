b8a9615b1018f26995a0ba63e4d7cca3efbe3a32
Konrad <konrad.gmur@gmail.com>
2020-08-18 19:14:41 +0200

    update BudgetTransactionList.js

--- src/pages/Budget/components/BudgetTransactionList/BudgetTransactionList.js
+++ src/pages/Budget/components/BudgetTransactionList/BudgetTransactionList.js
@@ -1,17 +1,32 @@
 import React from "react";
 import { connect } from "react-redux";
 import { List, ListItem } from "./BudgetTransactionList.css";
-import {groupBy} from 'lodash';
+import { groupBy } from "lodash";
 
 function BudgetTransactionList({ transactions }) {
-    const groupedTransactions = groupBy(transactions, transaction => )
+  const groupedTransactions = groupBy(transactions, (transaction) =>
+    new Date(transaction.date).getUTCDate()
+  );
+
   return (
     <List>
       <li>
-        <ul>
-          <li></li>
-        </ul>
+        {Object.entries(groupedTransactions).map(([key, transactions]) => (
+          <ul>
+            {transactions.map((transaction) => (
+              <ListItem>
+                <div>{transaction.description}</div>
+                <div>{transaction.amount}</div>
+                <div>{transaction.date}</div>
+                <div>{transaction.categoryId}</div>
+              </ListItem>
+            ))}
+          </ul>
+        ))}
       </li>
+      <ul>
+        <li></li>
+      </ul>
     </List>
   );
 }
